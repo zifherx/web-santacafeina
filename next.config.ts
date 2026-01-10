323ac958db582b72ee45d357385bd7f62f889478
Fernando Rojas <frojasq@yahoo.com>
2026-01-10 12:49:55 -0500

    [zifherx] Creando rama develop y ajustando el .gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  compress: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
